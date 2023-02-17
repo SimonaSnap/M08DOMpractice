04c46b897eec6a080af4b4436ae6a94c4d414d44
Simona <simona_s@outlook.com>
2023-02-16 22:00:34 -0800

    scope check

--- script.js
+++ script.js
@@ -24,10 +24,11 @@ submitBtn.textContent = "Add New Grocery Item"
 var newPos = document.getElementsByTagName("ul")[0];
 newPos.appendChild(inputEl);
 
+var addEl = document.createElement("li");
+var userInput = textBox.value;
+var addText = document.createTextNode(userInput);
+
 function addItem(){
-    var addEl = document.createElement("li");
-    var userInput = textBox.value;
-    var addText = document.createTextNode(userInput);
     console.log(userInput);
     addEl.appendChild(addText);
 
